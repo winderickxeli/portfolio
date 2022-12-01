15aca788adef883734a1082999e2febe89e6a197
winderickxeli <42890887+winderickxeli@users.noreply.github.com>
2022-12-01 21:37:52 +0100

    Dag 1: Basis CSS + planning. It sucks...

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Mijn portfolio
-## React.js
+## Plan van aanpak
+De eerste week zal ik vooral voorbereidingen treffen om onderdelen te tonen. In grote lijnen wil ik weten hoe alles er visueel uitziet en maak ik een planning op van wat ik wil bereiken.
+
+In de week van 5 december zal ik onderdelen van de cursus selecteren en implementeren en ook de centrale navigatiepagina voor deze opdrachten.
+
+De week erna zal vooral gefocused zijn op het maken van de contactpagina en het implementeren van de mailing service. Extra tijd zal gebruikt worden om een ``extra credits`` opdracht te maken.
+
+In de laatste week voor de kerstvakantie wil ik de applicatie in de lucht krijgen.
+
+Gedurende de kerstvakantie zal ik dan nog verder werken aan de ``extra credit`` opdrachten. Ook een extra ``theme`` button zal voorzien worden om de site van een donker naar een licht thema te brengen.
+
+## Technische onderdelen
+### React.js
 Dit portfolio is gemaakt met React.js en verder met Typescript. Deze Single Page Application zal ook gehost worden om in alle glorie te tonen wat in mijn mars heb. 
 
-## Vereisten
+### Vereisten
 Wil je deze SPA zelf ook draaien? Dan heb je een paar zaken nodig;
 - NodeJS
 - NPM
@@ -13,3 +25,15 @@ De applicatie start je uiteindelijk door het volgende uit te voeren in deze map:
 ````bash
 npm start
 ````
+
+## Visueel
+### Styling
+Omdat ik zelf nog een leercurve te doen heb met styling heb ik gekozen voor simpele css. Achteraf kan dit dan nog verder verhoogd worden naar een extra niveau door middel van een framework.
+
+### Setup
+De pagina is onderverdeeld in vier area's:
+- Navbar voor navigatie
+- Search om snel componenten terug te vinden
+- Body om alle componenten te tonen
+- Footer voor basis informatie
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,117 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Outlet, createBrowserRouter, RouterProvider, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import './Navbar.module.css';
+import './Search.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const Navbar = () => {
+  return(
+    <nav className='Navbar'>
+        <NavLink className="NavLink" to="/">
+          <img src={require("./images/ProfilePic.jpg")} alt="Profile picture" />
+        </NavLink>
+        <NavLink className="NavLink" to="/">About me</NavLink>
+        <NavLink className="NavLink" to="Assignments">Assignments</NavLink>
+        <NavLink className="NavLink" to="ExtraCredit">Extra credits</NavLink>
+        <NavLink className="NavLink" to="Contact">Contact me</NavLink>
+    </nav>
   );
+}
+
+const Root = () => {
+  return (
+    <body className='App'>
+      <div className='Nav'><Navbar /></div>
+      <div className='Search'><Search /></div>
+      <div className='Content'><Outlet /></div>
+      <div className='Footer'><Footer /></div>
+    </body>
+  )
+}
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Hello everyone!</h1>
+      <p>Welcome to my portfolio. I hope you find what you're looking for in these pages. If you're interested, leave a message and I'll get back to you.</p>
+    </main>
+  )
+}
+
+const Assignments = () => {
+  return (
+    <>Assignments</>
+  )
+}
+
+const ExtraCredit = () => {
+  return (
+    <>ExtraCredit</>
+  )
+}
+
+const Contact = () => {
+  return (
+    <>Contact</>
+  )
+}
+
+const Search = () =>{
+  return(
+    <div className='Search'>
+      <input type="text" value="Search here"/>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return(
+    <div>
+      <p>&copy; 2022 Eli Winderickx</p>
+    </div>
+  )
+}
+
+const AboutMe = () => {
+  return (
+    <article>
+      <h1>About me...</h1>
+      <p>I've been working as a system administrator for several years. During which I've seen changes in the industry. The sudden awareness that I need to adopt to a more DevOps centred mindset is just one of the reasons that made me decide to follow an IT development course at AP Highschool College.</p>
+      <p></p>
+    </article>
+  )
+}
+
+const App = () => {
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Root/>,
+  children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "Assignments",
+        element: <Assignments />
+      },
+      {
+        path: "ExtraCredit",
+        element: <ExtraCredit />
+      },
+      {
+        path: "Contact",
+        element: <Contact />
+      }
+  ]
+}])
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App;
