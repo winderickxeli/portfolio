d9341aaa6d42c80830a540612cc7bcbe1d190383
Eli Winderickx <eli.winderickx@student.ap.be>
2022-12-22 18:40:46 +0100

    Quote to footer and XKCD API

--- src/Functional/Home.tsx
+++ src/Functional/Home.tsx
@@ -30,41 +30,29 @@ const Home = () => {
     }
 
     const GetRandomComic = async () => {
-      let XkcdXml = await(await fetch("https://api.factmaven.com/xml-to-json?xml=https://xkcd.com/atom.xml")).json();
-      let XkcdComic = XkcdXml.feed.entry[0].summary.text
-
+      //fetch("https://api.factmaven.com/xml-to-json?xml=https://xkcd.com/atom.xml"
+      let XkcdComic = await(await fetch("https://xkcd.com/info.0.json", 
+      {
+        method: 'GET',
+        mode: 'no-cors',
+      })).json();
+      
       setComic({
         title: XkcdComic.title,
-        alt: XkcdComic.summary.text,
-        img: XkcdComic.summary.text
+        alt: XkcdComic.alt,
+        img: XkcdComic.img
       })
       console.log(XkcdComic);
-
-/*       let numberofComics = await parseInt(last.num);
-      let random = Math.floor(Math.random() * numberofComics + 1);
-      console.log(`https://xkcd.com/${random}/info.0.json`);
-      let result = await fetch(`https://xkcd.com/${random}/info.0.json`, {mode: "no-cors"});
-      let randomcomic = await result.json(); */
-/*       setComic({
-        title:last.title,
-        alt:last.alt,
-        img:last.img
-      }) */
     }
 
     GetRandomComic();
     getTweakersNews();
-    getQuote();
   },[]);
 
     return (
       <main className={styles.Content}>
         <h2>Welcome</h2>
         <p>This portfolio is part of an assignment of a course I'm attending. The course Web Frameworks is taught at AP Highschool college and teaches the React Framework. It is part of an Associate degree; Computer Programming. More information can be found on <a href="https://www.ap.be/graduaat/programmeren">the degree's page</a></p>
-        <article>
-          <h3>Quote of the day</h3>
-          <p className={styles.quote} >{quote}</p>
-        </article>
         <article>
         <h3>Latest by <a href="https://tweakers.net">Tweakers.net</a></h3>
           <table width="100%">
--- src/Webcomponents/Footer.tsx
+++ src/Webcomponents/Footer.tsx
@@ -1,10 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import styles from '../CSS/styles.module.css'
+import { useState, useEffect } from 'react';
 
 const Footer = () => {
+  const [quote, setQuote] = useState<string>("This is not the best landing page, it's just a tribute.");
+  useEffect(()=>{
+    const getQuote = async () => {
+      let quoteApi = await(await fetch("https://api.themotivate365.com/stoic-quote")).json();
+      setQuote(quote => quoteApi.quote);
+    }
+    getQuote();
+  },[])
+
     return(
       <footer className={styles.Footer}>
-        <NavLink to="Contact"><p>&copy; 2022 Eli Winderickx</p></NavLink>
+        <NavLink to="Contact"><p>&copy; 2022 Eli Winderickx<br/>{quote}</p></NavLink>
       </footer>
     )
   }
