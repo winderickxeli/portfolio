c4f32654e735aefb3dfa338ef5f56a3ace30ffcb
Eli Winderickx <42890887+winderickxeli@users.noreply.github.com>
2023-08-20 11:02:37 +0200

    Blog pagina verder aangevuld

--- src/Assignments/Blog/Blog.tsx
+++ src/Assignments/Blog/Blog.tsx
@@ -3,12 +3,13 @@ const Blog = () => {
         <main>
             <h2>Blog</h2>
             <p>Linux heeft me altijd heel sterk geinteresseerd. Het is een grote wereld vol met kennis die te leren vast. Het blijft echter een hele uitdaging om die kennis te vergaren en gestructureerd terug te vinden. Uiteraard kan je altijd een boek kopen en de kous is klaar maar toch zijn er twee goede redenen voor mij om een blog te onderhouden.</p>
-            <img src="/images/BlogScreenshot.png" />
+            <img src="/images/BlogScreenshot.png" width="100%"/>
             <ul>
                 <li>Documentatie: Naast veel bij te leren is het belangrijk die kennis vast te houden. Dat lukt enkel door middel van een goede documentatie.</li>
                 <li>Kennisdeling: Er bestaat heel wat kennis in het Engels over Linux maar in het Nederlands wordt dit niet veel gedaan.</li>
             </ul>
-
+            <p>Via een wekelijks stramien probeer ik nu nieuwe blogberichten aan te maken. Dat geeft mij telkens 7 dagen om me te verdiepen in een topic en vervolgens er een blogbericht over te schrijven. De topics gaan voornamelijk over onderdelen of applicaties binnen Linux alsook Zabbix.</p>
+            <p>Volg dus zeker mee via mijn <a href="https://www.linkedin.com/in/eli-winderickx-507944223">LinkedIn</a>, <a href="https://ieji.de/@Rubberducky456">Mastodon</a> of natuurlijk rechtstreeks op de <a href="https://blog.winderickx.me">Blogsite</a> zelf!</p>
         </main>
     )
 }
